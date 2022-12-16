64be7a28b7f5408fa4738337b42b8a760e755d81
Felix <fmohr@mail.upb.de>
2022-12-16 03:14:51 +0100

    Changed treatment of ds 4552

--- publications/jmlr2022/experiments/runexperiment.py
+++ publications/jmlr2022/experiments/runexperiment.py
@@ -35,7 +35,7 @@ def run_experiment(keyfields: dict, result_processor: ResultProcessor, custom_co
     
     # treat data sparse?
     binarize_sparse = openmlid in [1111, 4135, 4541, 41150, 42732, 42733, 42734]
-    drop = None if openmlid in [3, 38, 46, 57, 179, 180, 188, 231, 315, 897, 930, 953, 993, 1000, 1002, 1111, 1018, 1037, 1116, 1119, 1590, 4135, 4541, 40701, 40981, 41021, 41142, 41162, 41143, 42563, 42570, 42571, 42688, 42727, 42729, 42732, 42733, 42734] else 'first'
+    drop = None if openmlid in [3, 38, 46, 57, 179, 180, 188, 231, 315, 897, 930, 953, 993, 1000, 1002, 1111, 1018, 1037, 1116, 1119, 1590, 4135, 4541, 4552, 40701, 40981, 41021, 41142, 41162, 41143, 42563, 42570, 42571, 42688, 42727, 42729, 42732, 42733, 42734] else 'first'
     
     # Write intermediate results to database
     if is_classification:
@@ -59,8 +59,8 @@ if __name__ == '__main__':
         experimenter.execute(run_experiment, max_experiments=-1, random_order=True)
     else:
         run_experiment({
-            'openmlid': 1485,
-            'seed': 0,
+            'openmlid':  4552,
+            'seed': 7,
             'max_diff': 0.00001,
             'iterations_with_max_difff': 1000
         }, None, None)
