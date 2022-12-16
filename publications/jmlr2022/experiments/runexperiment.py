2791182159cabdf9556b00b33cdd7ebd845dc57c
Felix <fmohr@mail.upb.de>
2022-12-16 22:16:25 +0100

    added 33 to datasets for which all categorical attributes are preserved

--- publications/jmlr2022/experiments/runexperiment.py
+++ publications/jmlr2022/experiments/runexperiment.py
@@ -35,7 +35,7 @@ def run_experiment(keyfields: dict, result_processor: ResultProcessor, custom_co
     
     # treat data sparse?
     binarize_sparse = openmlid in [1111, 4135, 4541, 41150, 42732, 42733, 42734]
-    drop = None if openmlid in [3, 38, 46, 57, 179, 180, 188, 231, 315, 897, 930, 953, 993, 1000, 1002, 1111, 1018, 1037, 1116, 1119, 1590, 4135, 4541, 4552, 40701, 40981, 41021, 41142, 41162, 41143, 42563, 42570, 42571, 42688, 42727, 42729, 42732, 42733, 42734] else 'first'
+    drop = None if openmlid in [3, 33, 38, 46, 57, 179, 180, 188, 231, 315, 897, 930, 953, 993, 1000, 1002, 1111, 1018, 1037, 1116, 1119, 1590, 4135, 4541, 4552, 40701, 40981, 41021, 41142, 41162, 41143, 42563, 42570, 42571, 42688, 42727, 42729, 42732, 42733, 42734] else 'first'
     
     # Write intermediate results to database
     if is_classification:
@@ -54,12 +54,12 @@ def run_experiment(keyfields: dict, result_processor: ResultProcessor, custom_co
 if __name__ == '__main__':
     job_name = sys.argv[1]
     job_type = sys.argv[2]
-    if True:
+    if False:
         experimenter = PyExperimenter(experiment_configuration_file_path=f"config/experiments-fullforests-{job_type}.cfg", name = job_name)
         experimenter.execute(run_experiment, max_experiments=-1, random_order=True)
     else:
         run_experiment({
-            'openmlid':  354,
+            'openmlid':  202,
             'seed': 7,
             'max_diff': 0.00001,
             'iterations_with_max_difff': 1000
