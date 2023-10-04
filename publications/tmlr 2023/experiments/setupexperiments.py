8bdd41d5502be587bf72ffd756a3eba468cc277f
Felix <fmohr@mail.upb.de>
2023-10-04 02:50:17 +0200

    commented out regression configs

--- publications/tmlr 2023/experiments/setupexperiments.py
+++ publications/tmlr 2023/experiments/setupexperiments.py
@@ -6,8 +6,8 @@ experimenter = PyExperimenter(
 )
 experimenter.fill_table_from_config()
 
-experimenter = PyExperimenter(
-    experiment_configuration_file_path="config/experiments-fullforests-regression.cfg",
-    use_codecarbon=False
-)
-experimenter.fill_table_from_config()
+#experimenter = PyExperimenter(
+#    experiment_configuration_file_path="config/experiments-fullforests-regression.cfg",
+#    use_codecarbon=False
+#)
+#experimenter.fill_table_from_config()
