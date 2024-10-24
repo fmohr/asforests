bd95639fcf1312539dcf078bb7d5b3f8e3d98273
felix <felix@frank>
2024-10-24 20:23:10 +0200

    updated prediction script

--- publications/tmlr 2023/theorems/get_predictions_of_forest.py
+++ publications/tmlr 2023/theorems/get_predictions_of_forest.py
@@ -4,6 +4,7 @@ import pathlib
 
 from joblib import Parallel, delayed
 from sklearn.dummy import DummyClassifier
+from tqdm import tqdm
 
 import sys
 
@@ -19,13 +20,22 @@ if __name__ == "__main__":
         kwargs = {
             "max_depth": 1
         }
-        
+
+        print("Generating data")
         X_train, X_test, y_train, y_test = get_data_setup()
         Y_test = get_one_hot_encoding(y_test)
     
-        clf = ExtraTreesClassifier(n_estimators=10**5, random_state=seed, **kwargs)
-        clf.fit(X_train, y_train)
-        pd.DataFrame(clf.predict_proba(X_test[:10**5]), columns=["p1", "p2"]).to_csv(filename)
+        clf = ExtraTreesClassifier(n_estimators=0, random_state=seed, warm_start=True, **kwargs)
+
+        print("Ready. Now starting training")
+        for i in tqdm(range(100)):
+            clf.n_estimators += 10**3
+            clf.fit(X_train, y_train)
+        print(f"Finished training a forest with {len(clf)} trees")
+        print("Now getting predictions")
+        predictions = clf.predict_proba(X_test[:10**5])
+        print("Writing result file.")
+        pd.DataFrame(predictions, columns=["p1", "p2"]).to_csv(filename)
     
     else:
         print(f"File {filename} exists already. Skipping.")
