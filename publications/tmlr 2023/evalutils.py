dcfd9d75666c023fb90318879da45d490e7c17b4
felix <felix@frank>
2025-02-25 03:37:04 +0100

    updated database to run 5 seeds for each database

--- publications/tmlr 2023/evalutils.py
+++ publications/tmlr 2023/evalutils.py
@@ -91,7 +91,133 @@ def get_required_num_trees(prob_history, tree_score_history, eps, alpha):
                 return t   
         t += 1
 
-def build_full_classification_forest(openmlid, seed, alpha, eps):
+
+def get_performance_curve(
+        openmlid,
+        problem_type,
+        data_seed_application,
+        data_seed_training,
+        ensemble_seed,
+        eps,
+        patience
+    ):
+
+    if problem_type == "classification":
+        is_classification = True
+    elif problem_type == "regression":
+        is_classification = False
+    else:
+        ValueError(f"problem_type must be 'classification' or 'regression'")
+
+    print("Loading dataset")
+    X, y = get_dataset(openmlid)
+    print("Splitting the data")
+    X_inner, X_test, y_inner, y_test = sklearn.model_selection.train_test_split(
+        X,
+        y,
+        random_state=data_seed_application,
+        stratify=y if is_classification else None,
+        test_size=0.2
+    )
+    X_train, X_val, y_train, y_val = sklearn.model_selection.train_test_split(
+        X_inner,
+        y_inner,
+        random_state=data_seed_training,
+        stratify=y_inner if is_classification else None,
+        test_size=0.2
+    )
+
+    if is_classification:
+        train_labels = set(np.unique(y_train))
+        test_labels = set(np.unique(y_test))
+        train_labels_not_in_test = [l for l in train_labels if l not in test_labels]
+        test_labels_not_in_train = [l for l in test_labels if l not in train_labels]
+
+        print(
+            f"split created. {len(train_labels_not_in_test)} labels occur in train data but not in the test data, and {len(test_labels_not_in_train)} labels occur in the test data but not in the training data.\nNow building forest.")
+
+    # check whether we must pre-process
+    preprocessing = get_mandatory_preprocessing(X_train, y_train)
+    if preprocessing:
+        pl = sklearn.pipeline.Pipeline(preprocessing)
+        print(f"Modifying inputs with {pl}")
+        X_train = pl.fit_transform(X_train, y_train)
+        X_test = pl.transform(X_test)
+        eval_logger.info(f"Dataset sizes: {X_train.shape} for training and {X_test.shape} for test data.")
+
+    # prepare everything to efficiently compute the brier score
+    labels = list(np.unique(y))
+    n, k = len(y_test), len(labels)
+    Y_test = np.zeros((n, k))
+    for i, true_label in enumerate(y_test):
+        Y_test[i, labels.index(true_label)] = 1
+    Y_train = np.zeros((len(y_train), k))
+    for i, true_label in enumerate(y_train):
+        Y_train[i, labels.index(true_label)] = 1
+
+    def get_score(Y_true, Y_est):
+        return np.nanmean(np.sum((Y_est - Y_true) ** 2, axis=1))
+
+    memory_init = psutil.Process().memory_info().rss / (1024 * 1024)
+
+    Y_train_hat = np.zeros(Y_train.shape)
+    Y_test_hat = np.zeros(Y_test.shape)
+
+    t = 0
+
+    score_hist = []
+    step_size = 100 if len(X) < 10 ** 4 else 10
+
+    while True:
+
+        # create new forest (for speed ups)
+        eval_logger.info(f"Building {step_size} new trees.")
+        n_jobs = 1 if len(X) < 1000 or step_size < 8 else 8
+        rf = sklearn.ensemble.RandomForestClassifier(
+            n_estimators=step_size,
+            random_state=((3 * ensemble_seed) + 13) * (t + 1),
+            n_jobs=1
+        )
+        rf.fit(X_train, y_train)
+
+        scores_in_batch = []
+        for tree in rf:
+
+            # update posterior distribution on test set
+            start = time.time()
+            classes_ = tree.classes_
+            y_prob_test = tree.predict_proba(X_test) if is_classification else np.array([tree.predict(X_test)])
+
+            # prob_history_val.append(y_prob_test.round(4).astype(np.float16))
+
+            # if there are test labels not known to the forest, rearrange predictions
+            if len(rf.classes_) != len(labels) or any(rf.classes_ != labels):
+                eval_logger.warning(f"Adjusting labels!")
+                missing_labels = [l for l in labels if l not in rf.classes_]
+                missing_predictions = np.zeros((y_prob_test.shape[0], len(missing_labels)))
+                indices_augmented = (list(rf.classes_) + missing_labels)
+                indices = [indices_augmented.index(l) for l in labels]
+                y_prob_test = np.column_stack([y_prob_test, missing_predictions])[:, indices]
+
+            # update forest's prediction
+            t += 1
+            Y_test_hat += (y_prob_test - Y_test_hat) / t
+            memory_now = psutil.Process().memory_info().rss / (1024 * 1024) - memory_init
+
+            brier_score_val = get_score(Y_test, Y_test_hat)
+            score_hist.append(brier_score_val)
+
+        fluctuation = np.inf if len(score_hist) < patience else max(score_hist[-patience:]) - min(score_hist[-patience:])
+        eval_logger.info(f"Max score difference in batch is {fluctuation}")
+        if fluctuation <= eps:
+            break
+
+    eval_logger.info("Construction ready.")
+
+    return score_hist
+
+
+def build_full_classification_forest(openmlid, seed, min_steps_eps, eps):
     
     print("Loading dataset")
     X, y = get_dataset(openmlid)
@@ -99,8 +225,8 @@ def build_full_classification_forest(openmlid, seed, alpha, eps):
     X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state = seed)
     train_labels = set(np.unique(y_train))
     test_labels = set(np.unique(y_test))
-    train_labels_not_in_test = [l for l in train_labels if not l in test_labels]
-    test_labels_not_in_train = [l for l in test_labels if not l in train_labels]
+    train_labels_not_in_test = [l for l in train_labels if l not in test_labels]
+    test_labels_not_in_train = [l for l in test_labels if l not in train_labels]
                                
     print(f"split created. {len(train_labels_not_in_test)} labels occur in train data but not in the test data, and {len(test_labels_not_in_train)} labels occur in the test data but not in the training data.\nNow building forest.")
     
@@ -118,10 +244,10 @@ def build_full_classification_forest(openmlid, seed, alpha, eps):
     n, k = len(y_test), len(labels)
     Y_test = np.zeros((n, k))
     for i, true_label in enumerate(y_test):
-        Y_test[i,labels.index(true_label)] = 1
+        Y_test[i, labels.index(true_label)] = 1
     Y_train = np.zeros((len(y_train), k))
     for i, true_label in enumerate(y_train):
-        Y_train[i,labels.index(true_label)] = 1
+        Y_train[i, labels.index(true_label)] = 1
         
     def get_brier_score(Y, Y_prob):
         return np.nanmean(np.sum((Y_prob - Y)**2, axis=1))
@@ -137,11 +263,13 @@ def build_full_classification_forest(openmlid, seed, alpha, eps):
     
     required_trees = np.inf
     t = 1
-    prob_history_oob = []
-    prob_history_val = []
-    score_history = []
+    #prob_history_oob = []
+    #prob_history_val = []
+
     tree_score_history_oob = []
     tree_score_history_val = []
+    forest_score_history_oob = []
+    forest_score_history_val = []
     step_size = 100 if len(X) < 10**4 else 10
     
     times_train = []
@@ -154,24 +282,23 @@ def build_full_classification_forest(openmlid, seed, alpha, eps):
         # create new forest (for speed ups)
         eval_logger.info(f"Building {step_size} new trees.")
         n_jobs = 1 if len(X) < 1000 or step_size < 8 else 8
-        rf = asforests.RandomForestClassifier(step_size = step_size, random_state=t, n_jobs=n_jobs)
+        rf = asforests.RandomForestClassifier(step_size=step_size, random_state=t, n_jobs=n_jobs)
         gen = rf.get_score_generator(X_train, y_train)
         for inner_tree_id in range(step_size):
             y_prob_tree, train_time, pred_time, update_time = next(gen)
             times_train.append(train_time)
             times_predict_train.append(pred_time)
             times_update.append(update_time)
-            prob_history_oob.append(y_prob_tree.round(4).astype(np.float16))
-            #print(int(1000 * train_time), int(1000 * pred_time), int(1000 * update_time))
+            #prob_history_oob.append(y_prob_tree.round(4).astype(np.float16))
 
             # update posterior distribution on test set
             start = time.time()
             y_prob_test, classes_ = rf.predict_tree_proba(inner_tree_id, X_test)
-            prob_history_val.append(y_prob_test.round(4).astype(np.float16))
+            #prob_history_val.append(y_prob_test.round(4).astype(np.float16))
 
             # if there are test labels not known to the forest, rearrange predictions
             if len(rf.classes_) != len(labels) or any(rf.classes_ != labels):
-                missing_labels = [l for l in labels if not l in rf.classes_]
+                missing_labels = [l for l in labels if l not in rf.classes_]
                 missing_predictions = np.zeros((y_prob_test.shape[0], len(missing_labels)))
                 indices_augmented = (list(rf.classes_) + missing_labels)
                 indices = [indices_augmented.index(l) for l in labels]
@@ -185,22 +312,16 @@ def build_full_classification_forest(openmlid, seed, alpha, eps):
             times_predict_val.append(pred_time_val)
 
             memory_now = psutil.Process().memory_info().rss / (1024 * 1024) - memory_init
-            '''history.append((
-                int(np.round(10**6 * train_time)),
-                int(np.round(10**6 * pred_time)),
-                int(np.round(10**6 * pred_time_val)),
-                np.round(y_prob_tree, 4),
-                np.round(y_prob_test, 4),
-                np.round(memory_now, 1)
-            ))'''
 
             brier_score_oob_tree = get_brier_score(Y_train, y_prob_tree)
             brier_score_val_tree = get_brier_score(Y_test, y_prob_test)
             brier_score_oob = get_brier_score(Y_train, Y_train_hat)
             brier_score_val = get_brier_score(Y_test, Y_test_hat)
-            #score_history.append(brier_score_oob)
+
             tree_score_history_oob.append(brier_score_oob_tree)
             tree_score_history_val.append(brier_score_val_tree)
+            forest_score_history_oob.append(brier_score_oob)
+            forest_score_history_val.append(brier_score_val)
             t += 1
             
             eval_logger.info(
@@ -210,40 +331,44 @@ def build_full_classification_forest(openmlid, seed, alpha, eps):
                 f"Memory; {np.round(memory_now, 1)}MB."
             )
         
-        required_trees = get_required_num_trees(prob_history_oob, tree_score_history_oob, eps=eps, alpha=alpha)
-        eval_logger.info(f"New estimate for required number of trees: {required_trees}. Currently have info for {t}.")
-        if t >= required_trees:
+        #required_trees = get_required_num_trees(prob_history_oob, tree_score_history_oob, eps=eps, alpha=alpha)
+        #eval_logger.info(f"New estimate for required number of trees: {required_trees}. Currently have info for {t}.")
+        #if t >= required_trees:
+        #    break
+        #step_size = np.min([10**4, required_trees - t])
+        if t >= 100:
             break
-        step_size = np.min([10**4, required_trees - t])
 
-    eval_logger.info("Construction ready. Now compressing the history.")
+    eval_logger.info("Construction ready.")
 
+
+    """
     #oob_history = [e.tolist() for e in prob_history_oob]
     #val_history = [e.tolist() for e in prob_history_oob]
 
-    oob_history_as_string = str([e.tolist() for e in prob_history_oob])
-    val_history_as_string = str([e.tolist() for e in prob_history_val])
+    #oob_history_as_string = str([e.tolist() for e in prob_history_oob])
+    #val_history_as_string = str([e.tolist() for e in prob_history_val])
 
-    del prob_history_oob
-    del prob_history_val
+    #del prob_history_oob
+    #del prob_history_val
     gc.collect()
 
     oob_history_compressed = str(zlib.compress(oob_history_as_string.encode()))
     val_history_compressed = str(zlib.compress(val_history_as_string.encode()))
 
     del oob_history_as_string
-    del val_history_as_string
+    #del val_history_as_string
     gc.collect()
 
     eval_logger.info(f"History compressed, now returning.")
+    """
 
-    #eval_logger.info(f"History ready. Now returning.")
     return [
         X.tolist(),
         Y_train.tolist(),
         Y_test.tolist(),
-        oob_history_compressed,
-        val_history_compressed,
+        forest_score_history_oob,
+        forest_score_history_val,
         times_train,
         times_predict_train,
         times_predict_val,
