4d91a40d6df3b42e8ea0d5a6a993624aa51a94cb
Felix Mohr <mail@felixmohr.de>
2025-03-10 17:01:40 +0100

    clean up

--- publications/tmlr 2023/experiments/preliminary stuff/ground_truth/test.py
+++ publications/tmlr 2023/experiments/preliminary stuff/ground_truth/test.py
@@ -9,24 +9,22 @@ from tqdm import tqdm
 if __name__ == "__main__":
 
     gen = FiniteTaskGenerator(
-        num_possible_instances=20,
-        num_visible_instances=20,
-        num_possible_members=5,
+        num_possible_instances=10,
+        num_visible_instances=10,
+        num_possible_members=4,
         num_targets=3,
         method="finite_rf",
         ignore_missing_uniqueness=True,
         rs=np.random.RandomState(0)
     )
     gen.generate_deviations()
-    print(gen.deviation_mean, gen.deviation_var)
-    print(gen.deviation_cov_across_members)
-
+    
     target_mean = gen.deviation_mean
     target_var = gen.deviation_var
 
     epas = {
         m: EnsemblePerformanceAssessor(
-            upper_bound_for_sample_size=10**7,
+            upper_bound_for_sample_size=10**10,
             population_mode=m,
             estimate_deviation_covs=False
         )
@@ -40,7 +38,7 @@ if __name__ == "__main__":
     }
 
     matrices = []
-    for i in tqdm(range(10**4)):
+    for i in tqdm(range(10**7)):
         m = gen.sample_deviation_matrix()
         matrices.append(m)
         for epa_name, epa in epas.items():
@@ -48,19 +46,23 @@ if __name__ == "__main__":
             errors_mean[epa_name].append(np.abs(epa.gap_mean_point - target_mean))
             errors_var[epa_name].append(np.abs(epa.gap_var_point - target_var))
 
-    fig, axs = plt.subplots(1, 2, figsize=(10, 4), sharey=True)
+    fig, axs = plt.subplots(2, 2, figsize=(10, 7))
 
-    ax = axs[0]
-    for key, errors_for_key in errors_mean.items():
-        ax.plot(np.array(errors_for_key), label=key)
-    ax = axs[1]
-    for key, errors_for_key in errors_var.items():
-        ax.plot(np.array(errors_for_key), label=key)
+    for ax in axs[:, 0]:
+        for key, errors_for_key in errors_mean.items():
+            ax.plot(np.array(errors_for_key)[::10], label=key)
+    for ax in axs[:, 1]:
+        for key, errors_for_key in errors_var.items():
+            ax.plot(np.array(errors_for_key)[::10], label=key)
 
-    for ax in axs:
+    for ax in axs.flatten():
         ax.set_xscale("log")
-        ax.set_yscale("log")
         ax.grid()
+        ax.set_ylim([0, 0.1])
         ax.legend()
+
+    for ax in axs[1]:
+        ax.set_yscale("log")
         ax.set_ylim([10**-6, 1])
+
     plt.show()
