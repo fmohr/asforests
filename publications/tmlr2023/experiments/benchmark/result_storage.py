62dc1efa12a11205cac6718780b31b8b51741cb2
Felix Mohr <mail@felixmohr.de>
2025-03-17 16:20:55 +0100

    also updated other experiments for the limited scope

--- publications/tmlr2023/experiments/benchmark/result_storage.py
+++ publications/tmlr2023/experiments/benchmark/result_storage.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import json
+import collections.abc
 
 
 class ResultStorage:
@@ -29,11 +30,10 @@ class ResultStorage:
 
         # add known budgets
         if estimates is not None:
-            for p, estimates_for_p in estimates.items():
-                for a, estimates_for_a in estimates_for_p.items():
-                    for t, estimates_for_t in estimates_for_a.items():
-                        for b in estimates_for_t.keys():
-                            self._budgets.add(b)
+            for a, estimates_for_a in estimates.items():
+                for b in estimates_for_a.keys():
+                    assert isinstance(b, int), f"Budget '{b}' is not an integer and hence not a valid budget."
+                    self._budgets.add(b)
 
     @property
     def true_param_values(self):
@@ -80,6 +80,58 @@ class ResultStorage:
             data = json.load(src, object_pairs_hook=convert_keys_to_int)
         
         return cls(**data)
+
+    @classmethod
+    def merge(cls, stores):
+
+        if len(stores) < 2:
+            raise ValueError(f"Need at least two result storages to merge.")
+
+        # check that checkpoints coincide
+        t_checkpoints = None
+        true_param_values = None
+        for s in stores:
+            if t_checkpoints is None:
+                t_checkpoints = s.t_checkpoints
+                true_param_values = s.true_param_values
+            else:
+                if len(t_checkpoints) != len(s.t_checkpoints):
+                    raise ValueError("Cannot merge results storages with different checkpoints")
+                if np.any(t_checkpoints != s.t_checkpoints):
+                    raise ValueError("Cannot merge results storages with different checkpoints")
+                for param in true_param_values:
+                    if np.any(true_param_values[param] != s.true_param_values[param]):
+                        raise ValueError(f"Cannot merge results storages with different True parameter values for param {param}.")
+        if true_param_values is None:
+            raise ValueError(f"Cannot merge result stores with None for true_param_value")
+
+        # collect approach names
+        approach_names = set()
+        for s in stores:
+            approach_names |= set(s.approach_names)
+        approach_names = sorted(approach_names)
+
+        # merge estimates
+        estimates = {}
+        runtimes = {}
+        def deep_update(d, u):
+            for k, v in u.items():
+                if isinstance(v, collections.abc.Mapping) and k in d:
+                    deep_update(d[k], v)  # Recursive update for nested dictionaries
+                else:
+                    d[k] = v  # Overwrite for non-dictionaries
+            return d
+        for s in stores:
+            deep_update(estimates, s._estimates)
+            deep_update(runtimes, s._runtimes)
+        return ResultStorage(
+            approach_names=approach_names,
+            true_param_values=true_param_values,
+            t_checkpoints=t_checkpoints,
+            estimates=estimates,
+            runtimes=runtimes
+            )
+        
     
     def add_estimates(self, approach_name, budget, estimates_per_checkpoint, runtimes):
         
@@ -99,6 +151,14 @@ class ResultStorage:
                 self._estimates[approach_name][budget][t][p] = float(e)
         
         self._runtimes[approach_name][budget] = runtimes
+    
+    def rename_approach(self, n_from, n_to):
+        i = self.approach_names.index(n_from)
+        self.approach_names[i] = n_to
+        self._estimates[n_to] = self._estimates[n_from]
+        self._runtimes[n_to] = self._runtimes[n_from]
+        del self._estimates[n_from]
+        del self._runtimes[n_from]
     
     def get_estimates_from_approach_for_checkpoint(self, approach_name, t):
         results = []
