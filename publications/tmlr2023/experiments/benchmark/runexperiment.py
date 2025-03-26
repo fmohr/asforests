d04ddffe9daae1a208689d9a208debb5ccdd9fc0
Felix Mohr <mail@felixmohr.de>
2025-03-26 21:35:15 +0100

    updated experiment setup (1000 members and more repeats)

--- publications/tmlr2023/experiments/benchmark/runexperiment.py
+++ publications/tmlr2023/experiments/benchmark/runexperiment.py
@@ -15,65 +15,72 @@ import numpy as np
 ACCEPTED_APPROACHES = ["bootstrapping", "databaseperparameter", "parametricmodel"]
 
 def run_experiment(keyfields: dict, result_processor, custom_config):
-    openmlid = int(keyfields["openmlid"])
-    data_seed = 0
-    ensemble_sequence_seed = int(keyfields["ensemble_sequence_seed"])
-    num_possible_ensemble_members = int(keyfields["num_possible_ensemble_members"])
-    
-    training_instances_per_class = 10
-    validation_size = 50
 
-    b = Benchmark(
-        openmlid=openmlid,
-        data_seed=data_seed,
-        ensemble_seed=data_seed,
-        ensemble_sequence_seed=ensemble_sequence_seed,
-        num_possible_ensemble_members=num_possible_ensemble_members,
-        training_instances_per_class=training_instances_per_class,
-        validation_size=validation_size,
-        is_classification=True
-    )
-
-    # get generator for the estimates of the approach on the given problem
-    t_checkpoints = [10, 100, 1000]#, 10000]
-
-    approaches = {
-        "bootstrapping 1": BootstrappingApproach(num_resamples=1),
-        "bootstrapping 10": BootstrappingApproach(num_resamples=10),
-        "bootstrapping 100": BootstrappingApproach(num_resamples=100),
-        "parametric 1": ParametricModelApproach(num_simulated_ensembles=1),
-        "parametric 2": ParametricModelApproach(num_simulated_ensembles=2),
-        "parametric 4": ParametricModelApproach(num_simulated_ensembles=4),
-        "parametric 16": ParametricModelApproach(num_simulated_ensembles=16),
-        "parametric 64": ParametricModelApproach(num_simulated_ensembles=64),
-        "model free - stream": DatabaseWiseApproach(population_mode="stream"),
-        "model free - resample_no_replacement": DatabaseWiseApproach(population_mode="resample_no_replacement"),
-        "model free - resample_with_replacement": DatabaseWiseApproach(population_mode="resample_with_replacement")
-    }
-    
-    # define stream handler
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-
-    # configure logger
-    log_level = logging.WARN
-    logger = logging.getLogger("benchmark")
-    logger.handlers.clear()
-    logger.addHandler(ch)
-    ch.setLevel(log_level)
-    logger.setLevel(log_level)
-    
-    # run benchmark for 10 iterations (10 ensemble members)
-    print(f"Running experiment on dataset {openmlid} with seeds {data_seed}/{ensemble_sequence_seed}")
-    b.reset(approaches, t_checkpoints=t_checkpoints)
-    for _ in tqdm(range(2*10**2)):
-        b.step()
-    
     folder = f"results/"
     pathlib.Path(folder).mkdir(exist_ok=True, parents=True)
-    with open(f"{folder}/{openmlid}_{data_seed}_{ensemble_sequence_seed}_{num_possible_ensemble_members}_{training_instances_per_class}_{validation_size}.json", "w") as f:
-        b.result_storage.serialize(f)
+
+    openmlid = int(keyfields["openmlid"])
+    data_seed = 0
+    num_possible_ensemble_members = int(keyfields["num_possible_ensemble_members"])
+    validation_size = int(keyfields["validation_size"])
+    
+    training_instances_per_class = 10
+
+    for ensemble_sequence_seed in range(100):
+        filename = f"{folder}/{openmlid}_{data_seed}_{ensemble_sequence_seed}_{num_possible_ensemble_members}_{training_instances_per_class}_{validation_size}.json"
+        if pathlib.Path(filename).exists():
+            print(f"Skipping seed {ensemble_sequence_seed} since result file already exists.")
+            continue
+
+        b = Benchmark(
+            openmlid=openmlid,
+            data_seed=data_seed,
+            ensemble_seed=data_seed,
+            ensemble_sequence_seed=ensemble_sequence_seed,
+            num_possible_ensemble_members=num_possible_ensemble_members,
+            training_instances_per_class=training_instances_per_class,
+            validation_size=validation_size,
+            is_classification=True
+        )
+
+        # get generator for the estimates of the approach on the given problem
+        t_checkpoints = [10, 100, 1000]#, 10000]
+
+        approaches = {
+            "bootstrapping 1": BootstrappingApproach(num_resamples=1),
+            "bootstrapping 10": BootstrappingApproach(num_resamples=10),
+            "bootstrapping 100": BootstrappingApproach(num_resamples=100),
+            "parametric 1": ParametricModelApproach(num_simulated_ensembles=1),
+            "parametric 2": ParametricModelApproach(num_simulated_ensembles=2),
+            "parametric 4": ParametricModelApproach(num_simulated_ensembles=4),
+            "parametric 16": ParametricModelApproach(num_simulated_ensembles=16),
+            "parametric 64": ParametricModelApproach(num_simulated_ensembles=64),
+            "model free - stream": DatabaseWiseApproach(population_mode="stream"),
+            "model free - resample_no_replacement": DatabaseWiseApproach(population_mode="resample_no_replacement"),
+            "model free - resample_with_replacement": DatabaseWiseApproach(population_mode="resample_with_replacement")
+        }
+        
+        # define stream handler
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+
+        # configure logger
+        log_level = logging.WARN
+        logger = logging.getLogger("benchmark")
+        logger.handlers.clear()
+        logger.addHandler(ch)
+        ch.setLevel(log_level)
+        logger.setLevel(log_level)
+        
+        # run benchmark for 10 iterations (10 ensemble members)
+        print(f"Running experiment on dataset {openmlid} with seeds {data_seed}/{ensemble_sequence_seed}")
+        b.reset(approaches, t_checkpoints=t_checkpoints)
+        for _ in tqdm(range(10**3)):
+            b.step()
+        
+        with open(filename, "w") as f:
+            b.result_storage.serialize(f)
 
 
 if __name__ == "__main__":
