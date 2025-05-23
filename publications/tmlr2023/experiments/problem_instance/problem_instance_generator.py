2f71e23c2ca47134efcb36c5213824c30bd70135
Felix Mohr <mail@felixmohr.de>
2025-05-23 23:46:54 +0200

    adjusted the number of samples used for ground truth approximation to 10**6

--- publications/tmlr2023/experiments/problem_instance/problem_instance_generator.py
+++ publications/tmlr2023/experiments/problem_instance/problem_instance_generator.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
         raise ValueError(f"Please specify exactly two arguments (the job name and the number of cores to be used).")
     name = sys.argv[1]
     N_JOBS = int(sys.argv[2])
-    NUM_SAMPLES = 10**2
+    NUM_SAMPLES = 10**6
 
     pe = PyExperimenter(
         name=name,
