created cache for problem instance computation and corrected bug in the run script

publications/tmlr2023/experiments/problem_instance/_ground_truth_computer.py
@@ -1,9 +1,12 @@
 import itertools as it
 import pandas as pd
+import jsonlines
 from tqdm import tqdm
 import numpy as np
 from joblib import Parallel, delayed
+import pathlib
 
+import json, jsonlines
 import logging
 
 
@@ -53,7 +56,16 @@ class GroundTruthComputer:
             return self.get_all_ensemble_combinations_on_deviations(t=t)["z"].var(ddof=0 if bias else 1)
 
     
-    def sample_iid_scores(self, t_checkpoints, n_checkpoints=2, num_samples=10**6, num_samples_per_job=10**5, n_jobs=1, max_entries_in_batch_matrix = 10**8):
+    def sample_iid_scores(
+            self,
+            t_checkpoints,
+            n_checkpoints=2,
+            num_samples=10**6,
+            num_samples_per_job=10**5,
+            n_jobs=1,
+            max_entries_in_batch_matrix = 10**8,
+            cachefile=None
+        ):
         """
             This method approximates the true parameters in the iid setting by creating random samples of BOTH datasets and ensembles.
             This is a crucial difference to bootstrapping, which samples only in the ensemble space. 
@@ -86,6 +98,17 @@ class GroundTruthComputer:
         num_batches = int(np.ceil(num_samples_per_job / batch_size))
         n_bar = num_batches * len(t_checkpoints) * len(n_checkpoints)
 
+        # check whether we have a cache file
+        if cachefile is not None:
+            cachfile_path = pathlib.Path(cachefile)
+            if cachfile_path.exists():
+                with jsonlines.open(file=cachfile_path, mode="r") as reader:
+                    cache = list(reader)
+            else:
+                cache = []
+        else:
+            cache = None
+
         self.logger.info(
             f"Approximating ground truth on world with {self.deviations.shape[0]} ensemble members on {self.deviations.shape[1]} instances. "
             f"We will use {num_samples} samples of Z_nt for each out of {len(t_checkpoints) * len(n_checkpoints)} n-t-combinations. "
@@ -94,14 +117,27 @@ class GroundTruthComputer:
         
 
         def collect_scores_for_job(seed, n_checkpoints, t_checkpoints):
-
-            # create random state
-            random_state = np.random.RandomState(seed)
+            
+            cache_batches = []
 
             # outer loop over batches
             score_matrix = np.zeros((num_batches, batch_size, len(n_checkpoints), len(t_checkpoints)))
             pbar = tqdm(total=n_bar)
             for batch_idx in range(num_batches):
+
+                # create random state
+                random_state = np.random.RandomState(13 * (seed + 19 * batch_idx))
+
+                if cache is not None:
+                    cache_read = None
+                    for row in cache:
+                        if row[0] == seed and row[1] == batch_idx and row[2]:
+                            cache_read = np.array(row[3])
+                            break
+                    if cache_read is not None:
+                        score_matrix[batch_idx] = cache_read
+                        pbar.update(len(t_checkpoints) * len(n_checkpoints))
+                        continue
 
                 # compute the instance-wise value of Z_nt for all ensemble sizes simultaneously
                 for j, t in enumerate(t_checkpoints):
@@ -112,16 +148,38 @@ class GroundTruthComputer:
                         row_indices = np.arange(batch_size)[:, None]
                         score_matrix[batch_idx, :, i, j] = ensemble_errors_on_instances[row_indices, datasets_in_batch_of_size_n].mean(axis=1)
                         pbar.update(1)
+                
+                cache_batches.append([seed, batch_idx, True, score_matrix[batch_idx].tolist()])
+                if cachefile is not None and (batch_idx == num_batches - 1 or len(cache_batches) == 10):
+                    
+                    folder = cachfile_path.parent
+                    folder.mkdir(exist_ok=True, parents=True)
+
+                    # append result
+                    with cachfile_path.open("a") as f:
+                        for r in cache_batches:
+                            f.write(json.dumps(r) + "\n")
+                    cache_batches = []
+            
             pbar.close()
             return score_matrix.reshape(-1, *score_matrix.shape[2:])
         
         if n_jobs != 1 and num_sub_jobs > 1:
             results = Parallel(n_jobs=n_jobs, backend='loky')(delayed(collect_scores_for_job)(x, n_checkpoints, t_checkpoints) for x in range(num_sub_jobs))
         else:
-            results = [collect_scores_for_job(x, n_checkpoints, t_checkpoints) for x in range(num_sub_jobs)]
+            results = [collect_scores_for_job(seed=seed, n_checkpoints=n_checkpoints, t_checkpoints=t_checkpoints) for seed in range(num_sub_jobs)]
+
         return np.concatenate(results)
 
-    def sample_conditional_scores(self, t_checkpoints, num_samples=10**6, num_samples_per_job=10**5, n_jobs=1, max_entries_in_batch_matrix = 10**8):
+    def sample_conditional_scores(
+            self,
+            t_checkpoints,
+            num_samples=10**6,
+            num_samples_per_job=10**5,
+            n_jobs=1,
+            max_entries_in_batch_matrix=10**8,
+            cachefile=None
+        ):
         if num_samples_per_job is None:
             num_samples_per_job = num_samples
         num_sub_jobs = int(np.ceil(num_samples / num_samples_per_job))
@@ -139,6 +197,17 @@ class GroundTruthComputer:
         num_batches = int(np.ceil(num_samples_per_job / batch_size))
         n_bar = num_batches * len(t_checkpoints)
 
+        # check whether we have a cache file
+        if cachefile is not None:
+            cachfile_path = pathlib.Path(cachefile)
+            if cachfile_path.exists():
+                with jsonlines.open(file=cachfile_path, mode="r") as reader:
+                    cache = list(reader)
+            else:
+                cache = []
+        else:
+            cache = None
+
         self.logger.info(
             f"Approximating ground truth on world with {self.deviations.shape[0]} ensemble members on {self.deviations.shape[1]} instances. "
             f"We will use {num_samples} samples of Z_nt for each out of {len(t_checkpoints)} t-checkpoints. "
@@ -147,19 +216,45 @@ class GroundTruthComputer:
 
         def collect_scores_for_job(seed, t_checkpoints):
 
-            # create random state
-            random_state = np.random.RandomState(seed)
+            cache_batches = []
 
             # outer loop over batches
             score_matrix = np.zeros((num_batches, batch_size, len(t_checkpoints)))
             pbar = tqdm(total=n_bar)
             for batch_idx in range(num_batches):
 
+                # create random state
+                random_state = np.random.RandomState(13 * (seed + 19 * batch_idx))
+
+                if cache is not None:
+                    cache_read = None
+                    for row in cache:
+                        if row[0] == seed and row[1] == batch_idx and not row[2]:
+                            cache_read = np.array(row[3])
+                            break
+                    if cache_read is not None:
+                        score_matrix[batch_idx] = cache_read
+                        pbar.update(len(t_checkpoints))
+                        continue
+
                 # compute the instance-wise value of Z_nt for all ensemble sizes simultaneously
                 for j, t in enumerate(t_checkpoints):
                     ensembles_in_batch_of_size_t = random_state.randint(0, self.deviations.shape[0], size=(batch_size, t))
                     score_matrix[batch_idx, :, j] = (self.deviations[ensembles_in_batch_of_size_t].mean(axis=1)**2).mean(axis=1).sum(axis=1)
                     pbar.update(1)
+
+                cache_batches.append([seed, batch_idx, False, score_matrix[batch_idx].tolist()])
+                if cachefile is not None and (batch_idx == num_batches - 1 or len(cache_batches) == 10):
+                    
+                    folder = cachfile_path.parent
+                    folder.mkdir(exist_ok=True, parents=True)
+
+                    # append result
+                    with cachfile_path.open("a") as f:
+                        for r in cache_batches:
+                            f.write(json.dumps(r) + "\n")
+                    cache_batches = []
+
             pbar.close()
             return score_matrix.reshape(-1, *score_matrix.shape[2:])
         

publications/tmlr2023/experiments/problem_instance/problem_instance.py
@@ -171,7 +171,7 @@ class ProblemInstance:
     
     @property
     def means_cond(self):
-        if self._true_means_for_iid_case is None:
+        if self._true_means_for_cond_case is None:
             if self.exact_ground_truth_feasible:
                 self._compute_exact_ground_truth_cond()
             else:
@@ -183,7 +183,7 @@ class ProblemInstance:
     
     @property
     def vars_cond(self):
-        if self._true_means_for_iid_case is None:
+        if self._true_vars_for_cond_case is None:
             if self.exact_ground_truth_feasible:
                 self._compute_exact_ground_truth_cond()
             else:
@@ -287,7 +287,7 @@ class ProblemInstance:
         assert len(set(train_indices) | set(validation_indices) | set(oos_indices)) == len(self.X)
         if self.validation_size < 1:
             rel_validation_size = len(set(validation_indices)) / self.X.shape[0]
-            assert np.isclose(self.validation_size, rel_validation_size), f"There is a fraction of {rel_validation_size} instances marked for validation, but the parameter is {self.validation_size}"
+            assert np.isclose(self.validation_size, rel_validation_size, atol=0.005), f"There is a fraction of {rel_validation_size} instances marked for validation, but the parameter is {self.validation_size}"
         else:
             assert self.validation_size == len(set(validation_indices)), f"There are {len(set(validation_indices))} instances marked for validation, but the parameter is {self.validation_size}"
         self._indices_train = train_indices

publications/tmlr2023/experiments/problem_instance/problem_instance_generator.py
@@ -2,6 +2,7 @@ from experiments.problem_instance.problem_instance import ProblemInstance
 from experiments.problem_instance._ground_truth_computer import GroundTruthComputer
 from py_experimenter.experimenter import PyExperimenter
 from pathlib import Path
+import jsonlines
 
 from sklearn.datasets import fetch_openml
 import logging
@@ -71,6 +72,10 @@ def create_problem_instance_file_with_ground_truth_values(openmlid, data_seed, n
     )
     assert pi.predictions_val.shape[1] == validation_instances, f"Expected {validation_instances} validation instances, but ProblemInstance has {len(pi.predictions_val)}"
 
+
+    # get file
+    file = Path(f"tmp/{openmlid}/{data_seed}_{num_possible_ensemble_members}_{validation_instances}.jsonl")
+
     # approximate ground truth for IID case
     num_samples = num_samples_allowed_for_ground_truth_approximation
     num_samples_per_job = int(np.ceil(num_samples / n_jobs))
@@ -86,16 +91,20 @@ def create_problem_instance_file_with_ground_truth_values(openmlid, data_seed, n
         num_samples=num_samples_allowed_for_ground_truth_approximation,
         num_samples_per_job=num_samples_per_job,
         n_jobs=N_JOBS,
-        max_entries_in_batch_matrix=10**8
+        max_entries_in_batch_matrix=10**8,
+        cachefile=file
     )
     print(scores_iid.shape)
+
+    # approximate ground truth for conditional case
     gtc_cond = GroundTruthComputer(deviations=pi.deviations_val, logger=logger)
     scores_cond = gtc_cond.sample_conditional_scores(
         t_checkpoints=t_checkpoints,
         num_samples=num_samples_allowed_for_ground_truth_approximation,
         num_samples_per_job=num_samples_per_job,
         n_jobs=N_JOBS,
-        max_entries_in_batch_matrix=10**8
+        max_entries_in_batch_matrix=10**8,
+        cachefile=file
     )
     print(scores_cond.shape)
     t_end = time.time()
@@ -127,15 +136,15 @@ def create_problem_instance_file_with_ground_truth_values(openmlid, data_seed, n
 
 
 if __name__ == "__main__":
-    N_JOBS = 1
-    NUM_SAMPLES = 10**5
-    run_experiment(keyfields={
-        "openmlid": 1590,
-        "num_possible_ensemble_members": 256,
-        "data_seed": 9,
-        "validation_instances": 64
-    }, result_processor=None, custom_config=None)
-    exit(0)
+    #N_JOBS = 1
+    #NUM_SAMPLES = 10**4
+    # run_experiment(keyfields={
+    #     "openmlid": 61,
+    #     "num_possible_ensemble_members": 32,
+    #     "data_seed": 9,
+    #     "validation_instances": 64
+    # }, result_processor=None, custom_config=None)
+    # exit(0)
 
     if len(sys.argv) != 3:
         raise ValueError(f"Please specify exactly two arguments (the job name and the number of cores to be used).")