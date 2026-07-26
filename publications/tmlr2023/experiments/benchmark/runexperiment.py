80f47da1f8a962a41585335db9d49d2068680859
Felix Mohr <mail@felixmohr.de>
2026-07-26 11:41:48 +0200

    remaining experiments

--- publications/tmlr2023/experiments/benchmark/runexperiment.py
+++ publications/tmlr2023/experiments/benchmark/runexperiment.py
@@ -223,7 +223,7 @@ if __name__ == "__main__":
     pe = PyExperimenter(
         name="ensembles",
         use_codecarbon=False,
-        experiment_configuration_file_path=f"config/experiments.yaml"
+        experiment_configuration_file_path=f"config/experiments_full.yaml"
         )
 
     while True:
